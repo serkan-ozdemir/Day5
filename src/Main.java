dcc47abe62aa858d0744b695e4f4086b9f0896e4
serkan-ozdemir <56133743+serkan-ozdemir@users.noreply.github.com>
2021-07-27 20:57:37 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
     public static void main(String[] args) {
         UserManager userManager=new UserManager(new HibernateUserDao(),new GoogleRegisterManagerAdapter(),new VerificationManager(),new EmailManager());
         User user1 = new User(1,"Serkan","Özdemir" , "serkanozdmr7@gmail.com","1234567");
-        User user2 = new User(1,"S","Ö" , "serkanozdmr7@gmail.com","1234");
-        User user3 = new User(1,"S","Ö" , "serkanozdmr8@gmail.com","1234");
+        User user2 = new User(2,"S","Ö" , "serkanozdmr7@gmail.com","1234");
+        User user3 = new User(3,"S","Ö" , "serkanozdmr8@gmail.com","1234");
 
         userManager.register(user1);    //Basarili
         userManager.register(user2);    //Mail adresi kullaniliyor
